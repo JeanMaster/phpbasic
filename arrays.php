6cb9b3b0800a7cbdd4d152af4548d2bd88d6b81b
inversur <jeancarlos70@gmail.com>
2025-03-03 10:18:00 -0400

    cambio de nombres

---
+++ arrays.php
@@ -0,0 +1,25 @@
+<?php
+
+$frutas = ["manzanas", "naranjas" ,"peras" ]; 
+
+echo $frutas[2]; 
+
+
+echo "\n \n";
+
+
+print_r($frutas);
+
+//otra forma de declarar array
+
+$quesos = array("cheddar", "gouda", "mozarella"); 
+
+echo "\n \n";
+
+echo $quesos[2]; 
+
+
+echo "\n \n";
+
+
+print_r($quesos);
