99e6f3815bbf76879fca6ae62cb8dc957163b9ea
inversur <jeancarlos70@gmail.com>
2025-03-07 15:08:46 -0400

    finalizando curso

--- reto_caminos_tiendas.php
+++ reto_caminos_tiendas.php
@@ -1,12 +1,15 @@
 <?php
 
 
-$tiendas = [1 ,2 ,3 ,4 ,5,6,7,8] ;
+$temporal = 0; 
+$resultado = 0;
+$valor1 = 0 ; 
+$valor2 = 1 ;
 
-foreach ($tiendas as $tienda => $value) {
-    $tienda++;
-    $camino= $value +  $tienda;   
-    echo "$tienda $camino \n";
-
-};
-
+for ($i=2; $i <= 8 ; $i++) { 
+    $temporal = $valor2;
+    $resultado = $valor1 + $valor2;
+    $valor1 = $temporal;
+    $valor2 = $resultado;
+    echo "para ir de la tienda 1 a la tienda $i existen $resultado caminos \n";
+}
